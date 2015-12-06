f79266b5327edff0179019a82d3c40691c56b3a1
he wei wei <1830973318@qq.com>
2015-12-06 08:54:33 +0800

    这是我现阶段的工作

---
+++ 110101/the_3n1_problem.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int cycle_length(int n) {
+    int count=0;
+    while(1) {
+        if(n==1) break;
+        else {
+            if(n%2==0) n=n/2;
+            else n=3*n+1;
+            count++;
+        }
+    }
+    return count+1;
+}
+int find(int i,int j) {
+    int max=0;
+    int k;
+    //我这里的代码假设i比j小，但是这里题目没有做出这种要求！
+    for(k=i; k<=j; k++) {
+        int k_cycle_length=cycle_length(k);
+        if(k_cycle_length>max) max=k_cycle_length;
+    }
+    return max;
+}
+int main() {
+    int i,j;
+    /*printf("cycle length %d = %d\n",22,cycle_length(22));*/
+    while(scanf("%d%d",&i,&j)!=EOF) {
+	    int tmp_i=i;
+	    int tmp_j=j;
+        if(i>j) {
+            //swap i,j
+            int tmp=tmp_i;
+            tmp_i=tmp_j;
+            tmp_j=tmp;
+        }
+        int max=find(tmp_i,tmp_j);
+        printf("%d %d %d\n",i,j,max);
+    }
+    return 0;
+}
---
+++ 110102/minesweeper.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#define N 100
+#define M 100
+int main() {
+    int n,m;
+    int i,j;
+    char fields[M][N];
+    int mines[M][N];
+
+    int count=1;
+    while(1) {
+        scanf("%d%d",&n,&m);
+	//test all input is right!
+        //printf("n=%d,m=%d\n",n,m);
+        if(n==0 && m==0) break;
+        getchar();//delete newline after n,m
+        printf("Field #%d:\n",count);
+        for(i=0; i<n; i++) {
+            for(j=0; j<m; j++) {
+                fields[i][j]=getchar();
+            }
+            getchar();
+        }
+	/*
+        for(i=0; i<n; i++) {
+            for(j=0; j<m; j++) {
+                printf("%c",fields[i][j]);
+            }
+            printf("\n");
+        }
+	*/
+
+        for(i=0; i<n; i++) {
+            for(j=0; j<m; j++) {
+                if(fields[i][j]=='*') mines[i][j]=-1;
+                else {
+                    mines[i][j]=0;
+                    if(i+1<n && fields[i+1][j]=='*') mines[i][j]++;
+                    if(j+1<n && fields[i][j+1]=='*') mines[i][j]++;
+                    if(i-1>=0 && fields[i-1][j]=='*') mines[i][j]++;
+                    if(j-1>=0 && fields[i][j-1]=='*') mines[i][j]++;
+                    if(j-1>=0 && i-1>=0 && fields[i-1][j-1]=='*') mines[i][j]++;
+                    if(j+1<n && i+1<n && fields[i+1][j+1]=='*') mines[i][j]++;
+                    if(j+1<n && i-1>=0 && fields[i-1][j+1]=='*') mines[i][j]++;
+                    if(j-1>=0 && i+1<n && fields[i+1][j-1]=='*') mines[i][j]++;
+                }
+            }
+        }
+        for(i=0; i<n; i++) {
+            for(j=0; j<m; j++) {
+                if(mines[i][j]==-1)printf("*");
+                else printf("%d",mines[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        count++;
+    }
+    return 0;
+}
